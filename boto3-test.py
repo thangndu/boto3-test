d8c25754ea867f1d2ddc431ef55e1b31d957ad48
Nguyen <DucThang.Nguyen@emc.com>
2020-08-30 20:13:20 +0700

    changed

--- boto3-test.py
+++ boto3-test.py
@@ -21,14 +21,31 @@ def main():
 
     # upload file
 
+    metadata = {
+        'file-name': 'piper.png',
+        'file-owner': 'thangndu'
+        }
+
+    #The metadata can be seen in S3 in the "http headers" tab
+    #It gets stored as "x-amz-meta-object-metadata:piper"
+
+
     file_name = "piper.png"
     f = open(file_name,'rb')
 
     print("uploading file...")
-    s3.put_object(Bucket=bucket_name, Key=file_name, Body=f.read())
+    s3.put_object(Bucket=bucket_name, Key=file_name, Body=f.read(), Metadata=metadata)
     print("file uploaded!")
 
     f.close()
+
+    # change permission
+    # s3.put_object_acl(Bucket=bucket_name, Key=file_name, ACL='public-read')
+    # print("file permission changed to public-read")
+
+    # retrive object metadata
+    #response = s3.head_object(Bucket=bucket_name, Key=file_name)
+    #print(response)
 
 if __name__ == "__main__":
     main()
